58064714f397ef864f7d2804dbd15089e0542868
Tiago Fernandes <fohalvutlun@gmail.com>
2021-05-03 15:20:34 +0100

    Consolidated individual functionality tests

--- test/index.js
+++ test/index.js
@@ -1,1 +1,15 @@
-import('./test.submit-questionnaire-responses.js');
+import testSubmitQuestionnaireResponsesUC from './submit-questionnaire-responses/index.js';
+import testSummarizeQuestionnaireResponsesUC from './summarize-questionnaire-responses/index.js';
+
+let test_counter = 0;
+const begin = new Date();
+console.log(`[TEST - START] Started testing at ${begin.toISOString()}.`)
+
+
+test_counter = await testSubmitQuestionnaireResponsesUC(test_counter);
+test_counter = await testSummarizeQuestionnaireResponsesUC(test_counter);
+
+
+const end = new Date();
+const elapsed = end.getTime() - begin.getTime();
+console.log(`[TEST - END] Completed testing at ${end.toISOString()}. \n\t Completed a total of ${test_counter} tests in ${elapsed} ms`);
