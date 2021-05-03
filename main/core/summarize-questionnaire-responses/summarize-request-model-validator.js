34ec2bb535ed6621e084a06d73b5b0e69300ad43
Tiago Fernandes <fohalvutlun@gmail.com>
2021-05-03 13:27:24 +0100

    Fixed function name

--- main/core/summarize-questionnaire-responses/summarize-request-model-validator.js
+++ main/core/summarize-questionnaire-responses/summarize-request-model-validator.js
@@ -1,4 +1,4 @@
-export default function makeSubmissionRequestValidator({
+export default function makeSummarizeRequestValidator({
     summarizeRequestModelValidatorGateway,
     jtdSchemaValidator
 }) {
